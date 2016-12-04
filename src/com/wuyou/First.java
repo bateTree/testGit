458c2a04908e56cb79b87d594fff926c6addddb5
bateTree <179421362@qq.com>
2016-12-04 23:29:31 +0800

    master

--- src/com/wuyou/First.java
+++ src/com/wuyou/First.java
@@ -4,5 +4,8 @@ public class First {
 	public static void main(){
 		System.out.println("思想他的爱，思想他的良善");
 		System.out.println("思想祂恩典");
+		System.out.println("领我一生");
+		System.out.println("就像天离地有多高");
+		System.out.println("我天父的慈爱也有多么深");
 	}
 }
